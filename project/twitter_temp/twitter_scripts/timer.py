d53f470298fbe52418caffe4425cd71c076f32d1
Nuno Miguel Mota Goncalves <motagonc@iccluster060.iccluster.epfl.ch>
2017-12-17 18:05:26 +0100

    Adds timer class

---
+++ project/twitter_temp/twitter_scripts/timer.py
@@ -0,0 +1,42 @@
+from datetime import datetime, timedelta
+
+class Timer:
+
+    _timer_label = None
+    _start_time = None
+    _end_time = None
+
+    def __init__(self, label):
+        self._timer_label = label
+
+    def _get_current_time():
+    	return datetime.now()
+
+    def _get_time_delta():
+    	return self._end_time - self._start_time
+
+    def start(self):
+        self._start_time = _get_current_time()
+
+    def stop(self):
+    	self._end_time = _get_current_time()
+
+    def __repr__(self):
+        overall_str_format = '[{} Days : {} Hours : {} Minutes : {} Seconds]'
+        overall_time_delta = _get_time_delta()
+        return overall_str_format.format(
+        	overall_time_delta.days,
+        	overall_time_delta.hours,
+        	overall_time_delta.minutes,
+        	overall_time_delta.seconds 
+        )
+
+    def __str__(self):
+        overall_str_format = '[{} Days : {} Hours : {} Minutes : {} Seconds]'
+        overall_time_delta = _get_time_delta()
+        return overall_str_format.format(
+        	overall_time_delta.days,
+        	overall_time_delta.hours,
+        	overall_time_delta.minutes,
+        	overall_time_delta.seconds 
+        )
