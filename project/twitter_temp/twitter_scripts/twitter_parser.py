f4bfcb4055bd4898fa64d2a0988659d381b375f3
Nuno Miguel Mota Goncalves <motagonc@iccluster060.iccluster.epfl.ch>
2017-12-17 16:22:00 +0100

    Separates code into functions

--- project/twitter_temp/twitter_scripts/twitter_parser.py
+++ project/twitter_temp/twitter_scripts/twitter_parser.py
@@ -22,83 +22,91 @@ sqlContext = SQLContext(sc)
 # Add modules for UDFs
 sc.addPyFile('/home/motagonc/ada2017-hw-private/project/scripts/language_recognition.py')
 
-# statistics initialization
-statistics = Statistics('Twitter Filter', False)
-
 # Fetch data
 log_print('Fetching data from local dataset')
 twitter_df, ucdp_df = dh.fetch_data('local', sc)
 
-# Removing unnecessary columns
-twitter_df = twitter_df.drop(twitter_df['User']).drop(twitter_df['ID'])
+def filter_twitter_df(twitter_df):
 
-# Removing rows with null values (we NEED values for every column)
-twitter_df = twitter_df.dropna()
+	# defining necessary UDFs
+	is_tweet_english_udf = udf(lr.is_tweet_english, BooleanType())
 
-# Filter english language tweets
-log_print('Filtering \'en\' language entries')
-statistics.set_stage('Pre-defined laguage filter')
+	# statistics initialization
+	statistics = Statistics('Twitter Filter', False)
 
-statistics.add_stats('Before', twitter_df)
-twitter_df = twitter_df.filter(twitter_df['Language'] == 'en').drop(twitter_df['Language'])
-statistics.add_stats('After', twitter_df)
+	# Removing unnecessary columns
+	filtered_twitter_df = twitter_df.drop(twitter_df['User']).drop(twitter_df['ID'])
 
-# Parse date format (avoid UDFs because of serialization/desirialization overhead)
-''' 
-Format ignores the Time-Zone component. This is not a problem for a couple of reasons:
+	# Removing rows with null values (we NEED values for every column)
+	filtered_twitter_df = filtered_twitter_df.dropna()
 
-1. In this case we can increase the parsing/filtering speed of our algorithm
-2. It doesn't really affect our results since it simply will leave more information
-at filtering stage
-3. After filtering, we can define our margin of error as being (+/-) 25h 10m (the largest
-existing time zone difference between Napari and Samoa)
-4. We can always adjust our 'time-frame' to account for these differences
-5. Since our timeframe will always be greater than 48h, and we care mainly about the emotional
-transition from before/after a conflict, regardless of time-zone, we will never ignore Tweets
-that belong to a critical window of (+/-) 22h 50m around the conflict.
-6. A critical window of (+/-) 22h 50m around an event will allow for an overall timeframe
-of 45h 40m, larger than the biggest time-zone difference - so regardless of the time of day people
-might be more active on Twitter, we will always account for their activity 
-'''
-log_print('Parsing date format')
-twitter_date_format = "EEE MMM dd HH:mm:ss '+'SSSS yyyy"
-twitter_df = twitter_df.withColumn('Timestamp', unix_timestamp(twitter_df['Date'], twitter_date_format).cast('timestamp')) \
-			.drop(twitter_df['Date'])
+	# Filter english language tweets
+	log_print('Filtering \'en\' language entries')
+	statistics.set_stage('Pre-defined laguage filter')
+	statistics.add_stats('Before', filtered_twitter_df)
+	filtered_twitter_df = filtered_twitter_df.filter(filtered_twitter_df['Language'] == 'en').drop(filtered_twitter_df['Language'])
+	statistics.add_stats('After', filtered_twitter_df)
 
-ucdp_date_format = "yyyy-MM-dd"
-ucdp_df = ucdp_df.withColumn('Timestamp', unix_timestamp(ucdp_df['Date Start'], ucdp_date_format).cast('timestamp')) \
-			.drop(ucdp_df['Date Start']) \
-			.drop(ucdp_df['Date End'])
-'''
-log_print('Separating date into \'day\', \'month\' and \'year\' columns')
-twitter_df = twitter_df.withColumn('Day', dayofmonth(twitter_df['Parsed Date']).cast('string')) \
-			.withColumn('Month', month(twitter_df['Parsed Date']).cast('string')) \
-			.withColumn('Year', year(twitter_df['Parsed Date']).cast('string')) \
-			.drop(twitter_df['Parsed Date'])
-'''
+	# Parse date format (avoid UDFs because of serialization/desirialization overhead)
+	''' 
+	Format ignores the Time-Zone component. This is not a problem for a couple of reasons:
 
-log_print('Filtering english tweets')
-statistics.set_stage('Custom laguage filter')
-is_tweet_english_udf = udf(lr.is_tweet_english, BooleanType())
+	1. In this case we can increase the parsing/filtering speed of our algorithm
+	2. It doesn't really affect our results since it simply will leave more information
+	at filtering stage
+	3. After filtering, we can define our margin of error as being (+/-) 25h 10m (the largest
+	existing time zone difference between Napari and Samoa)
+	4. We can always adjust our 'time-frame' to account for these differences
+	5. Since our timeframe will always be greater than 48h, and we care mainly about the emotional
+	transition from before/after a conflict, regardless of time-zone, we will never ignore Tweets
+	that belong to a critical window of (+/-) 22h 50m around the conflict.
+	6. A critical window of (+/-) 22h 50m around an event will allow for an overall timeframe
+	of 45h 40m, larger than the biggest time-zone difference - so regardless of the time of day people
+	might be more active on Twitter, we will always account for their activity 
+	'''
+	log_print('Parsing date format')
+	twitter_date_format = "EEE MMM dd HH:mm:ss '+'SSSS yyyy"
+	filtered_twitter_df = filtered_twitter_df.withColumn('Timestamp', unix_timestamp(filtered_twitter_df['Date'], twitter_date_format).cast('timestamp')) \
+				.drop(filtered_twitter_df['Date'])
 
-statistics.add_stats('Before', twitter_df)
-twitter_df = twitter_df.filter(is_tweet_english_udf(twitter_df['Content']))
-statistics.add_stats('After', twitter_df)
+	log_print('Filtering english tweets')
+	statistics.set_stage('Custom laguage filter')
+	statistics.add_stats('Before', filtered_twitter_df)
+	filtered_twitter_df = filtered_twitter_df.filter(is_tweet_english_udf(filtered_twitter_df['Content']))
+	statistics.add_stats('After', filtered_twitter_df)
 
-log_print('Filtering by year')
-statistics.set_stage('Custom year filter')
-twitter_df = twitter_df.filter(year(twitter_df['Timestamp']) == 2015)
-ucdp_df = ucdp_df.filter(year(ucdp_df['Timestamp']) == 2015)
+	'''
+	log_print('Filtering on time window')
+	statistics.set_stage('Custom timestamp filter')
+	statistics.add_stats('Before', filtered_twitter_df)
+	filtered_twitter_df = filtered_twitter_df.join(ucdp_df, datediff(filtered_twitter_df['Timestamp'], ucdp_df['Timestamp']) <= DEFAULT_TIME_WINDOW, 'inner')
+	statistics.add_stats('After', filtered_twitter_df)
+	'''
 
-log_print('Filtering on time window')
-statistics.set_stage('Custom timestamp filter')
+	return (filtered_twitter_df, statistics)
 
-statistics.add_stats('Before', twitter_df)
-twitter_df = twitter_df.join(ucdp_df, datediff(twitter_df['Timestamp'], ucdp_df['Timestamp']) <= DEFAULT_TIME_WINDOW, 'inner')
-statistics.add_stats('After', twitter_df)
+def filter_ucdp_df(ucdp_df):
+
+	# statistics initialization
+	statistics = Statistics('UCDP Filter', False)
+
+	log_print('Parsing date format')
+	ucdp_date_format = "yyyy-MM-dd"
+	filtered_ucdp_df = ucdp_df.withColumn('Timestamp', unix_timestamp(ucdp_df['Date Start'], ucdp_date_format).cast('timestamp')) \
+				.drop(ucdp_df['Date Start']) \
+				.drop(ucdp_df['Date End'])
+
+	return (filtered_ucdp_df, statistics)
+
+# Filter dataframes and collect statistics
+twitter_df, twitter_statistics = filter_twitter_df(twitter_df)
+ucdp_df, ucdp_statistics = filter_ucdp_df(ucdp_df)
 
 # Print statistics
-print(statistics)
+if twitter_statistics._is_enabled == True:
+	print(twitter_statistics)
+if ucdp_statistics._is_enabled == True:
+	print(ucdp_statistics)
 
 # Display 5 entries
 twitter_df.show(5)
