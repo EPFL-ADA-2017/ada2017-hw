d7267263051ac34540aad1b098938bd0d847e8d3
Nuno Miguel Mota Goncalves <motagonc@iccluster060.iccluster.epfl.ch>
2017-12-17 18:57:56 +0100

    Fixes timer string representation

--- project/twitter_temp/twitter_scripts/timer.py
+++ project/twitter_temp/twitter_scripts/timer.py
@@ -9,36 +9,44 @@ class Timer:
     def __init__(self, label):
         self._timer_label = label
 
-    def _get_current_time():
+    def _get_current_time(self):
     	return datetime.now()
 
-    def _get_time_delta():
+    def _get_time_delta(self):
     	return self._end_time - self._start_time
 
     def start(self):
-        self._start_time = _get_current_time()
+        self._start_time = self._get_current_time()
 
     def stop(self):
-    	self._end_time = _get_current_time()
+    	self._end_time = self._get_current_time()
 
     def __repr__(self):
-        overall_str_format = '[{}] {} Days : {} Hours : {} Minutes : {} Seconds'
-        overall_time_delta = _get_time_delta()
+        overall_str_format = '{} [{} Days, {} Hours, {} Minutes, {} Seconds]'
+        overall_time_delta = self._get_time_delta()
+
+        overall_time_delta_days = overall_time_delta.days
+        overall_time_delta_hours, remainder = divmod(overall_time_delta.seconds, 3600)
+        overall_time_delta_minutes, overall_time_delta_seconds = divmod(remainder, 60)
         return overall_str_format.format(
         	self._timer_label,
-        	overall_time_delta.days,
-        	overall_time_delta.hours,
-        	overall_time_delta.minutes,
-        	overall_time_delta.seconds 
+        	overall_time_delta_days,
+        	overall_time_delta_hours,
+        	overall_time_delta_minutes,
+        	overall_time_delta_seconds 
         )
 
     def __str__(self):
-        overall_str_format = '[{}] {} Days : {} Hours : {} Minutes : {} Seconds'
-        overall_time_delta = _get_time_delta()
+        overall_str_format = '{} [{} Days, {} Hours, {} Minutes, {} Seconds]'
+        overall_time_delta = self._get_time_delta()
+        
+        overall_time_delta_days = overall_time_delta.days
+        overall_time_delta_hours, remainder = divmod(overall_time_delta.seconds, 3600)
+        overall_time_delta_minutes, overall_time_delta_seconds = divmod(remainder, 60)
         return overall_str_format.format(
         	self._timer_label,
-        	overall_time_delta.days,
-        	overall_time_delta.hours,
-        	overall_time_delta.minutes,
-        	overall_time_delta.seconds 
+        	overall_time_delta_days,
+        	overall_time_delta_hours,
+        	overall_time_delta_minutes,
+        	overall_time_delta_seconds 
         )
