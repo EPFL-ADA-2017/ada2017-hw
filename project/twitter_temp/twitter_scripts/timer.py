c3999ee650d24cfb9f6fa3c0735b563614d0c0be
Nuno Miguel Mota Goncalves <motagonc@iccluster060.iccluster.epfl.ch>
2017-12-18 20:25:30 +0100

    Cleans up and comments timer

--- project/twitter_temp/twitter_scripts/timer.py
+++ project/twitter_temp/twitter_scripts/timer.py
@@ -1,3 +1,13 @@
+'''
+The Timer class is a simple version of an actual 
+timer. When started it stores the starting time 
+and when stopped it stores the stopping time.
+
+Both __repr__ and __str__ methods have been
+overriden to allow easier and clearer prints.
+'''
+
+# Imports
 from datetime import datetime, timedelta
 
 class Timer:
@@ -6,22 +16,53 @@ class Timer:
     _start_time = None
     _end_time = None
 
+    # Constructor
     def __init__(self, label):
         self._timer_label = label
 
+    '''
+    Private Methods
+    '''
     def _get_current_time(self):
+        '''
+        This method returns the current
+        time as a datetime object.
+        '''
     	return datetime.now()
 
     def _get_time_delta(self):
+        '''
+        This method returns the timedelta
+        between two datetime objects.
+        '''
     	return self._end_time - self._start_time
 
+    '''
+    Public Methods
+    '''
     def start(self):
+        '''
+        This method is used to start
+        the Timer object.
+        '''
         self._start_time = self._get_current_time()
 
     def stop(self):
+        '''
+        This method is used to stop
+        the Timer object.
+        '''
     	self._end_time = self._get_current_time()
 
+    '''
+    Methods' Override
+    '''
     def __repr__(self):
+        '''
+        This method overrides the default
+        object representation for the Timer
+        class' objects.
+        '''
         overall_str_format = '{} [{} Days, {} Hours, {} Minutes, {} Seconds]'
         overall_time_delta = self._get_time_delta()
 
@@ -37,6 +78,11 @@ class Timer:
         )
 
     def __str__(self):
+        '''
+        This method overrides the default
+        object string representation for 
+        the Timer class' objects.
+        '''
         overall_str_format = '{} [{} Days, {} Hours, {} Minutes, {} Seconds]'
         overall_time_delta = self._get_time_delta()
         
