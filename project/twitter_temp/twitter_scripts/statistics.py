66efc31e7c2065f52ad9a2bd8cd49556d4ca27e3
Nuno Miguel Mota Goncalves <motagonc@iccluster060.iccluster.epfl.ch>
2017-12-18 20:30:35 +0100

    Cleans up and comments statistics

--- project/twitter_temp/twitter_scripts/statistics.py
+++ project/twitter_temp/twitter_scripts/statistics.py
@@ -1,3 +1,10 @@
+'''
+This class was created as a container
+for statistical information. Each Statistics
+object should represent a single operation
+and can be internally separated in 'stages'.
+'''
+
 class Statistics:
 
     _statistics_label = None
@@ -5,16 +12,39 @@ class Statistics:
     _cur_stage = None
     _data = None
 
+    # Constructor
     def __init__(self, label, enabled):
         self._statistics_label = label
         self._is_enabled = enabled
         self._cur_stage = 'Default'
         self._data = {}
 
+    '''
+    Private Methods
+    '''
+    def _get_dataframe_count(self, dataframe):
+        '''
+        This method returns the approximate
+        number of elements in a dataframe.
+        '''
+        return dataframe.rdd.countApprox(timeout=1000)
+
+    '''
+    Public Methods
+    '''
     def set_stage(self, stage_name):
+        '''
+        This method updates the current
+        stage.
+        '''
         self._cur_stage = stage_name
 
     def add_stats(self, label, dataframe):
+        '''
+        This method adds a certain dataframe's
+        statistics to the currently selected
+        stage.
+        '''
     	if (self._is_enabled == False):
     		return
 
@@ -22,14 +52,24 @@ class Statistics:
         stage_data[label] = '# Rows: {}'.format(self._get_dataframe_count(dataframe))
         self._data[self._cur_stage] = stage_data
 
-    def _get_dataframe_count(self, dataframe):
-        return dataframe.rdd.countApprox(timeout=1000)
-
+    '''
+    Overriden Methods
+    '''
     def __repr__(self):
+        '''
+        This method overrides the default
+        object representation for the 
+        Statistics class' objects.
+        '''
         overall_str_format = '{}: {}'
         return overall_str_format.format(self._statistics_label, list(self._data.keys()))
 
     def __str__(self):
+        '''
+        This method overrides the default
+        object string representation for 
+        the Statistics class' objects.
+        '''
         overall_str_format = '---{}---{}\n---------------------'
         stage_str_format = '\n|\n| {}:\n|{}'
         stat_str_format = '\n| \t[{}] {}'
