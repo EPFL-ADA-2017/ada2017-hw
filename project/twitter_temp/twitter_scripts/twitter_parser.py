10c10851b00315d8bf9362908e14502bfbf66118
Nuno Miguel Mota Goncalves <motagonc@iccluster060.iccluster.epfl.ch>
2017-12-18 19:57:24 +0100

    Cleans up and comments twitter parser

--- project/twitter_temp/twitter_scripts/twitter_parser.py
+++ project/twitter_temp/twitter_scripts/twitter_parser.py
@@ -1,3 +1,32 @@
+'''
+The purpose of this script is to fetch the necessary data from 
+the Twitter-leon dataset. Below are stated some of the 
+assumptions taken throughout the process.
+
+While parsing Twitters string-encoded dates, we ignore the 
+time-zone's offset. This is not a problem because:
+
+	1. It avoids the overhead of converting everything into the 
+	same timezone
+    2. The same day accross all time-zones represents more data
+    thatn if we simply considered one (so we are not losing 
+    valuable data)
+    3. The amount of extra data can be defined as (+/-) 25h 10m,
+    which is the largest existing time zone difference in the world
+    (between Napari and Samoa)
+    4. We can always adjust the time-frame we are considering to
+    adjust for these diferences
+    5. Since our time-frame is set to 48h, and we ultimately care
+    about the contrast on the information before/after a certain 
+    date, we will never ignore Tweets that belong to a critical 
+    window of (+/-) 22h 50m around the conflict
+    6. A critical window of (+/-) 22h 50m around a certain conflict 
+    will allow for an overall time-frame of 45h 40m - larger than 
+    the largest time-zone difference - which means that regardless 
+    of the time of day people might be more active on Twitter, we 
+    will always account for their activity
+'''
+
 # Add scripts folder to path
 import sys
 sys.path.append('/home/motagonc/ada2017-hw-private/project/scripts')
@@ -14,7 +43,7 @@ from logger import log_print
 import language_recognition as lr
 import data_handler as dh
 
-# context initialization
+# Context initialization
 sc = SparkContext()
 sqlContext = SQLContext(sc)
 
@@ -26,13 +55,18 @@ log_print('Fetching data from datasets')
 twitter_df, ucdp_df = dh.fetch_data('remote', sc)
 
 def filter_twitter_df(twitter_df):
-
+	'''
+	This method is used to apply initial filters
+	on the Twitter dataframe. It receives the 
+	previous dataframe as a parameter and returns 
+	a new filtered dataframe.
+	'''
 	log_print('>> Start >> Filtering Twitter dataframe')
 
-	# defining necessary UDFs
+	# Defining necessary UDFs
 	is_tweet_english_udf = udf(lr.is_tweet_english, BooleanType())
 
-	# statistics initialization
+	# Statistics initialization
 	statistics = Statistics('Twitter Filter', False)
 
 	# Removing unnecessary columns
@@ -48,28 +82,13 @@ def filter_twitter_df(twitter_df):
 	filtered_twitter_df = filtered_twitter_df.filter(filtered_twitter_df['Language'] == 'en').drop(filtered_twitter_df['Language'])
 	statistics.add_stats('After', filtered_twitter_df)
 
-	# Parse date format (avoid UDFs because of serialization/desirialization overhead)
-	''' 
-	Format ignores the Time-Zone component. This is not a problem for a couple of reasons:
-
-	1. In this case we can increase the parsing/filtering speed of our algorithm
-	2. It doesn't really affect our results since it simply will leave more information
-	at filtering stage
-	3. After filtering, we can define our margin of error as being (+/-) 25h 10m (the largest
-	existing time zone difference between Napari and Samoa)
-	4. We can always adjust our 'time-frame' to account for these differences
-	5. Since our timeframe will always be greater than 48h, and we care mainly about the emotional
-	transition from before/after a conflict, regardless of time-zone, we will never ignore Tweets
-	that belong to a critical window of (+/-) 22h 50m around the conflict.
-	6. A critical window of (+/-) 22h 50m around an event will allow for an overall timeframe
-	of 45h 40m, larger than the biggest time-zone difference - so regardless of the time of day people
-	might be more active on Twitter, we will always account for their activity 
-	'''
+	# Parse date format
 	log_print('Parsing date format')
 	twitter_date_format = "EEE MMM dd HH:mm:ss '+'SSSS yyyy"
 	filtered_twitter_df = filtered_twitter_df.withColumn('Timestamp', unix_timestamp(filtered_twitter_df['Date'], twitter_date_format).cast('timestamp')) \
 				.drop(filtered_twitter_df['Date'])
 
+	# Filtering English Tweets
 	log_print('Filtering english tweets')
 	statistics.set_stage('Custom laguage filter')
 	statistics.add_stats('Before', filtered_twitter_df)
@@ -81,12 +100,18 @@ def filter_twitter_df(twitter_df):
 	return (filtered_twitter_df, statistics)
 
 def filter_ucdp_df(ucdp_df):
-
+	'''
+	This method is used to apply initial filters
+	on the UCDP dataframe. It receives the 
+	previous dataframe as a parameter and returns 
+	a new filtered dataframe.
+	'''
 	log_print('>> Start >> Filtering UCDP dataframe')
 
-	# statistics initialization
+	# Statistics initialization
 	statistics = Statistics('UCDP Filter', False)
 
+	# Parse date format
 	log_print('Parsing date format')
 	ucdp_date_format = "yyyy-MM-dd"
 	filtered_ucdp_df = ucdp_df.withColumn('Timestamp_2', unix_timestamp(ucdp_df['Date Start'], ucdp_date_format).cast('timestamp')) \
@@ -97,17 +122,25 @@ def filter_ucdp_df(ucdp_df):
 	return (filtered_ucdp_df, statistics)
 
 def merge_dataframes_on_time_window(twitter_df, ucdp_df):
+	'''
+	This method is used to merge both Twitter
+	and UCDP filtered dataframes into a single
+	one - based on wether or not a Tweet' timestamp
+	is within a certain time window. This can be
+	achieved through an inner join.
+	'''
 	log_print('>> Start >> Filtering on time window')
 
-	# statistics initialization
+	# Statistics initialization
 	statistics = Statistics('Twitter/UCDP Merging', False)
 
-	# constants
+	# Constants
 	DEFAULT_TIME_WINDOW = 2
 
+	# Inner join based on time window
 	statistics.set_stage('Custom timestamp filter')
 	statistics.add_stats('Before', twitter_df)
-	merged_df = twitter_df.join(ucdp_df, pyspark_abs(datediff(twitter_df['Timestamp'], ucdp_df['Timestamp_2'])) <= DEFAULT_TIME_WINDOW, 'inner')
+	merged_df = twitter_df.join(ucdp_df, (datediff(twitter_df['Timestamp'], ucdp_df['Timestamp_2']) <= DEFAULT_TIME_WINDOW) & (datediff(twitter_df['Timestamp'], ucdp_df['Timestamp_2']) >= -DEFAULT_TIME_WINDOW), 'inner')
 	statistics.add_stats('After', merged_df)
 
 	log_print('<<  End  << Filtering on time window')
@@ -122,14 +155,8 @@ timer.start()
 twitter_df, twitter_statistics = filter_twitter_df(twitter_df)
 ucdp_df, ucdp_statistics = filter_ucdp_df(ucdp_df)
 
-# Further filter dataframe
-ucdp_df = ucdp_df.withColumn('Year', year(ucdp_df['Timestamp_2'])).withColumn('Month', month(ucdp_df['Timestamp_2'])).withColumn('Day', dayofmonth(ucdp_df['Timestamp_2']))
-ucdp_unique_years = ucdp_df.select('Year').distinct().rdd.map(lambda x: x[0]).collect()
-ucdp_unique_months = ucdp_df.select('Month').distinct().rdd.map(lambda x: x[0]).collect()
-log_print('Unique years found in UCDP: {}'.format(ucdp_unique_years))
-log_print('Unique months found in UCDP: {}'.format(ucdp_unique_months))
-log_print('Filtering Twitter based on UCDP dates')
-twitter_df = twitter_df.filter((year(twitter_df['Timestamp']).isin(ucdp_unique_years)) & (month(twitter_df['Timestamp']).isin(ucdp_unique_months)))
+# Cache UCDP to speed up merging
+ucdp_df.cache()
 
 # Merge dataframes based on time window
 merged_df, merged_statistics = merge_dataframes_on_time_window(twitter_df, ucdp_df)
@@ -137,15 +164,15 @@ merged_df, merged_statistics = merge_dataframes_on_time_window(twitter_df, ucdp_
 # Print statistics
 log_print(twitter_statistics)
 log_print(ucdp_statistics)
-#log_print(merged_statistics)
+log_print(merged_statistics)
 
 # Display 5 entries
 log_print(twitter_df)
 log_print(ucdp_df)
-#log_print(merged_df)
+log_print(merged_df)
 
 # save merged results to disk
-#dh.save_data(merged_df, 'motagonc_merged_df_sample')
+dh.save_data(merged_df.limit(100), 'motagonc_merged_df_sample')
 
 # Display timer
 timer.stop()
